c5f65a0d4680ba8fcef93f4b5b3613a51243b48e
Chin Kuay <kuaychin@gmail.com>
2020-04-06 13:37:12 -0500

    added store.js

--- src/store.js
+++ src/store.js
@@ -1,1 +1,13 @@
+import { createStore } from "redux";
 
+const initialState = {};
+
+function reducer(state = initialState, action) {
+  const { type, payload } = action;
+  switch (type) {
+    default:
+      return state;
+  }
+}
+
+export default createStore(reducer);
